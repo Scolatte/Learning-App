508dfe4dd10e0f186a1ed401496a4296b0478bc7
Kerim <abdulkerimzengin111@gmail.com>
2023-05-16 11:39:41 +0300

    Update QuizManager.cs

--- Final Project/Assets/QuizManager.cs
+++ Final Project/Assets/QuizManager.cs
@@ -71,7 +71,7 @@ public class QuizManager : Singleton<QuizManager>
 
     private IEnumerator EndQuiz()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
 
         LessonManager.Instance.QuizPage.SetActive(false);
 
