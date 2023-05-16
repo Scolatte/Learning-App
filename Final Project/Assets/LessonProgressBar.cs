e50c0f3237964bd56e418bc55d478d4ac285976d
Kerim <abdulkerimzengin111@gmail.com>
2023-05-16 23:49:26 +0300

    Update LessonProgressBar.cs

--- Final Project/Assets/LessonProgressBar.cs
+++ Final Project/Assets/LessonProgressBar.cs
@@ -19,7 +19,19 @@ public class LessonProgressBar : Singleton<LessonProgressBar>
 
     public void CalculatePositions(List<RectTransform> _rectTransforms)
     {
-        Debug.Log("MÝN = " + _rectTransforms[0].offsetMin.x);
-        Debug.Log("Max = " + _rectTransforms[0].offsetMax.x);
+        //Debug.Log("MÝN = " + _rectTransforms[0].offsetMin.x);
+        //Debug.Log("Max = " + -_rectTransforms[0].offsetMax.x);
+
+        //Debug.Log("MÝN = " + _rectTransforms[1].offsetMin.x);
+        //Debug.Log("Max = " + -_rectTransforms[1].offsetMax.x);
+
+        for (int i = 0; i < _rectTransforms.Count; i++)
+        {
+            float left = 0f;
+            float right = 0f;
+
+            _rectTransforms[i].offsetMin = new Vector2(left ,_rectTransforms[i].offsetMin.y);
+            _rectTransforms[i].offsetMax = new Vector2(-right, _rectTransforms[i].offsetMax.y);
+        }
     }
 }
